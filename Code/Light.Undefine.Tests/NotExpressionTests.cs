31026646089479619ad4a50c41172b16a345ba6e
Kenny Pflug <kenny.pflug@live.de>
2018-09-05 20:41:14 +0200

    added NotExpression

---
+++ Code/Light.Undefine.Tests/NotExpressionTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Light.Undefine.Tests
+{
+    public static class NotExpressionTests
+    {
+        [Fact]
+        public static void DerivesFromPreprocessorExpression() => typeof(NotExpression).Should().BeDerivedFrom<PreprocessorExpression>();
+
+        [Theory]
+        [MemberData(nameof(NotDefinedData))]
+        public static void NotDefined(string[] definedSymbols) =>
+            new NotExpression(new Symbol("NETSTANDARD1_0")).Evaluate(definedSymbols).Should().BeTrue();
+
+        public static readonly TheoryData<string[]> NotDefinedData =
+            new TheoryData<string[]>
+            {
+                new[] { "DEBUG" },
+                new[] { "NETSTANDARD2_0", "NET45", "NET40" }
+            };
+
+        [Theory]
+        [MemberData(nameof(DefinedData))]
+        public static void Defined(string[] definedSymbols) => 
+            new NotExpression(new Symbol("NET45")).Evaluate(definedSymbols).Should().BeFalse();
+
+        public static readonly TheoryData<string[]> DefinedData =
+            new TheoryData<string[]>
+            {
+                new[] { "NET45" },
+                new[] { "NET45", "NETSTANDARD2_0" },
+                new[] { "NETSTANDARD2_0", "NET45", "SL5" }
+            };
+    }
+}
---
+++ Code/Light.Undefine/NotExpression.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Light.GuardClauses;
+
+namespace Light.Undefine
+{
+    public sealed class NotExpression : PreprocessorExpression
+    {
+        public NotExpression(PreprocessorExpression expression) => Expression = expression.MustNotBeNull(nameof(expression));
+
+        public PreprocessorExpression Expression { get; }
+        public override bool Evaluate(IEnumerable<string> definedSymbols) => !Expression.Evaluate(definedSymbols);
+    }
+}
